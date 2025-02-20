2b7669c161e6ac3361a6ca9c809ab3ec93b1a47b
Lachlan Rehder <lachlan.rehder@outlook.com>
2025-02-20 22:14:03 +1000

    Moved to using async version of fs functions for 'build' command

--- cli/build.ts
+++ cli/build.ts
@@ -1,20 +1,26 @@
 import { AppError } from "./index.ts";
-import { execSync } from "child_process";
-import { cpSync, mkdirSync, rmSync } from "fs";
+import { exec } from "child_process";
+import { cp, mkdir, rm, type NoParamCallback } from "fs";
 
 /**
  * Build Merge into the `dist` directory.
  *
  * @throws {AppError} If the build fails.
  */
-export default function () {
+export default async function () {
 	try {
-		rmSync("dist", { recursive: true, force: true });
-		mkdirSync("dist", { recursive: true });
-		cpSync("src/pages", "dist", { recursive: true });
-		cpSync("assets", "dist", { recursive: true });
-		execSync("tsc");
-		execSync("sass src/styles:dist/styles --no-source-map");
+		// delete any previous builds
+		await promisify(callback => rm("dist", { recursive: true, force: true }, callback));
+		await promisify(callback => mkdir("dist", { recursive: true }, callback));
+
+		// build project asynchronously
+		const promises = [
+			promisify(callback => cp("src/pages", "dist", { recursive: true }, callback)),
+			promisify(callback => cp("assets", "dist", { recursive: true }, callback)),
+			promisify(callback => exec("tsc", callback)),
+			promisify(callback => exec("sass src/styles:dist/styles --no-source-map", callback))
+		];
+		await Promise.all(promises);
 	} catch (error) {
 		if (error instanceof Error) {
 			throw new AppError(`Could not build Merge: ${error.message}`);
@@ -22,4 +28,25 @@ export default function () {
 			throw error;
 		}
 	}
+}
+
+/** Type of the resolvers returned by {@link Promise.withResolvers()} excluding the {@link Promise}. */
+type Resolvers<T> = Omit<PromiseWithResolvers<T>, "promise">;
+type Callback = (error: Error | null) => void;
+
+/**
+ * Convert a NodeJS async-callback operation to a {@link Promise}.
+ *
+ * @param fn The function containing the async-callback operator.
+ * @returns The {@link fn} Converted into a {@link Promise}.
+ */
+function promisify(fn: (callback: Callback) => void) {
+	const { promise, resolve, reject } = Promise.withResolvers<void>();
+	const callback = (error: Error) => {
+		if (error === null) resolve();
+		else reject(error);
+	};
+
+	fn(callback);
+	return promise;
 }
